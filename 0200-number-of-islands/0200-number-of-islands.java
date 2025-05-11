e9ee82d837941a5b818edd629e46326354c4e612
kimjaehyeonn <jhjhh023@gmail.com>
2025-05-11 22:23:52 +0900

    Time: 7 ms (18.28%), Space: 52.7 MB (10.52%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,56 +1,59 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
-    // 4방향 이동 (상, 하, 좌, 우)
-    int[] dr = {-1, 1, 0, 0};
-    int[] dc = {0, 0, -1, 1};
+    //1. 전역 변수 설정
+    int[] dr = { 0,0,1,-1};
+    int[] dc = { 1,-1,0,0};
 
+    // 2. main
     public int numIslands(char[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        boolean[][] visited = new boolean[n][m];
-        int count = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        int answer= 0;
 
-        // 모든 셀 순회
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < m; c++) {
-                // 육지이고 방문 안 했으면 BFS 시작
-                if (grid[r][c] == '1' && !visited[r][c]) {
-                    bfs(grid, visited, r, c);
-                    count++;
-                }
-            }
+
+        for (int r = 0; r < m; r++ ){
+            for (int c = 0; c<n; c++){
+                if(grid[r][c]=='1' && !visited[r][c]){
+
+                bfs(grid, visited, r, c);
+                answer++;
+            }}
         }
-        return count;
+    return answer;
     }
 
-    private void bfs(char[][] grid, boolean[][] visited, int sr, int sc) {
-        int n = grid.length;
-        int m = grid[0].length;
-
+    public void bfs(char[][] grid, boolean[][] visited, int sr, int sc) {
+        int m = grid.length;
+        int n = grid[0].length;
+        
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{sr, sc});
+        queue.offer(new int[] {sr,sc});
         visited[sr][sc] = true;
 
-        while (!queue.isEmpty()) {
+        while( !queue.isEmpty()) {
             int[] current = queue.poll();
             int r = current[0];
             int c = current[1];
 
-            for (int i = 0; i < 4; i++) {
+             for ( int i =0; i < 4; i++) {
                 int nr = r + dr[i];
                 int nc = c + dc[i];
+                if(isValid(nr, nc, m,n  ) && grid[nr][nc] == '1' && !visited[nr][nc]) {
+                visited[nr][nc] = true;
+                queue.offer(new int[] {nr,nc});
+                
+             }
+             }
 
-                if (isValid(nr, nc, n, m) && grid[nr][nc] == '1' && !visited[nr][nc]) {
-                    visited[nr][nc] = true;
-                    queue.offer(new int[]{nr, nc});
-                }
-            }
+             
         }
+
     }
 
-    private boolean isValid(int r, int c, int n, int m) {
-        return r >= 0 && r < n && c >= 0 && c < m;
+    public boolean isValid(int r, int c, int m , int n ) {
+        return 0<= r && r< m &&  0<= c && c < n;
     }
+
 }
