3e96b9ed7ccfef6daf0628b90916cbe5148b0844
kimjaehyeonn <jhjhh023@gmail.com>
2025-05-17 21:31:07 +0900

    [Bronze V] Title: A+B, Time: 172 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+import static javax.management.Query.plus;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println(x+y);
+        sc.close();
+
+    }
+
+}
