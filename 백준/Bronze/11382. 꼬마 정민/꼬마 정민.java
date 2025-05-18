a67fdc0739dcae5933bd212e9d73160e8033a1f4
kimjaehyeonn <jhjhh023@gmail.com>
2025-05-18 23:09:50 +0900

    [Bronze V] Title: 꼬마 정민, Time: 168 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        String input = sc.nextLine();
+        String[] input = sc.nextLine().split(" ");
 
-        long a = Long.parseLong(input.split(" ")[0]);
-        long b = Long.parseLong(input.split(" ")[1]);
-        long c = Long.parseLong(input.split(" ")[2]);
+        long a = Long.parseLong(input[0]);
+        long b = Long.parseLong(input[1]);
+        long c = Long.parseLong(input[2]);
         
         System.out.println(a+b+c);
         sc.close();
