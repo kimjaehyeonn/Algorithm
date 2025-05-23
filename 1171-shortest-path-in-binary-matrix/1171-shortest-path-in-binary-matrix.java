2665d77e1c2f0193aad774cd1e3df65b6c883d5a
kimjaehyeonn <jhjhh023@gmail.com>
2025-05-23 17:32:11 +0900

    Time: 18 ms (39.4%), Space: 45.2 MB (91.72%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
@@ -1,21 +1,17 @@
-class Solution {
-    int[] dr = { 0, 0, 1, -1, 1, -1, 1,-1};
-    int[] dc = { 1,-1, 0, 1, -1, 0, 1,-1};
-
-    public boolean isValid (int r, int c, int n ){
-        return 0<= r && r < n && 0<= c && c < n;
-    }
+class Solution { 
+    int[] dr = { 1, 1, 1, -1, -1, -1, 0, 0};
+    int[] dc = { 0, 1, -1, 0, 1, -1, 1, -1};
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n = grid.length;
-        Queue<int[]> queue = new LinkedList<>();
         boolean[][] visited = new boolean[n][n];
-
-        if (grid[0][0] == 1 || grid[n-1][n-1]==1) {
+        
+        if (grid[0][0]==1 || grid[n-1][n-1]==1) {
             return -1;
-        } 
+        }
 
-        queue.offer(new int[] { 0,0,1});
-        visited[0][0] = true;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {0,0,1});
+        visited[0][0] =true;
 
         while(!queue.isEmpty()) {
             int[] current = queue.poll();
@@ -26,18 +22,20 @@ class Solution {
             if (r == n-1 && c == n-1) {
                 return d;
             }
-
-            for ( int i =0; i < 8; i++) {
+            for (int i=0; i<8; i++) {
                 int nr = r + dr[i];
                 int nc = c + dc[i];
 
-                if(isValid(nr,nc,n) && grid[nr][nc]==0 && !visited[nr][nc] ){
-                    visited[nr][nc] = true;
-                    queue.offer(new int[] {nr,nc,d+1});   
+                if(isValid(nr,nc,n)&& !visited[nr][nc] && grid[nr][nc]==0) {
+                    visited[nr][nc]= true;
+                    queue.offer(new int[] {nr,nc,d+1});
                 }
             }
-
         }
-return -1;
+        return -1;
+    }
+    
+     private boolean isValid (int r, int c, int n){
+        return 0<= r && r < n && 0<= c && c<n;
     }
 }
