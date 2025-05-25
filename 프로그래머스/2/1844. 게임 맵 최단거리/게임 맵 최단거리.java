1001b84d8c29a0fa5ca349ee8de7be0c21e39a8b
kimjaehyeonn <jhjhh023@gmail.com>
2025-05-25 16:30:33 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.41 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,45 +1,42 @@
 import java.util.*;
 class Solution {
-    int[] dr = {1,-1,0, 0};
-    int[] dc = { 0,0, 1, -1};
-    
+    int[] dr = { 1, -1, 0 ,0};
+    int[] dc = {0,0,1,-1};
     public int solution(int[][] maps) {
-        int m = maps.length;
-        int n = maps[0].length;
-        boolean[][] visited = new boolean[m][n];
-        
-        if (maps[0][0] == 0 || maps[m-1][n-1] ==0 ) {
-            return -1;
-        }
+        int n = maps.length;
+        int m = maps[0].length;
+        boolean[][] visited = new boolean[n][m];
         
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {0,0,1});
+        queue.offer(new int[]{0,0,1});
         visited[0][0] = true;
+        
         
         while(!queue.isEmpty()) {
             int[] current = queue.poll();
-            int r = current[0] ;
+            int r = current[0];
             int c = current[1];
             int d = current[2];
-             if (r == m - 1 && c == n - 1) {
+            
+            if (r == n-1 && c == m-1) {
                 return d;
             }
-
             
-            for (int i =0; i<4; i++){
+            for(int i=0; i< 4; i++){
                 int nr = r + dr[i];
-                int nc = c + dc[i];
+                int nc= c + dc[i];
                 
-                if (isValid(nr, nc, m ,n ) && !visited[nr][nc] && maps[nr][nc]==1) {
+                if (isValid(nr, nc,  n,m) && !visited[nr][nc] && maps[nr][nc]==1 ){
+                    queue.offer(new int[] { nr, nc, d+1});
                     visited[nr][nc] = true;
-                    queue.offer(new int[]{nr,nc,d+1});
-                    
                 }
             }
         }
         return -1;
+        
+        
     }
-    private boolean isValid(int r, int c, int m ,int n) {
-        return 0<=r && r<m && 0<=c && c<n;
+    private boolean isValid(int r, int c, int n, int m) {
+        return 0<=r&& r<n && 0<=c && c<m;
     }
 }
