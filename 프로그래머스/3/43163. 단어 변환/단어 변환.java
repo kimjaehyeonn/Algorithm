b298711edd50daeb5f48a4680a402f3c0b0eef9a
kimjaehyeonn <jhjhh023@gmail.com>
2025-05-27 00:30:52 +0900

    [level 3] Title: 단어 변환, Time: 0.68 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,44 +1,42 @@
 import java.util.*;
 class Solution {
     
-    class Words {
-        String word;
-        int count;
-        Words(String word, int count) {
-            this.word = word;
-            this.count = count;
+    class Words{
+    String node;
+        int edge;
+        Words(String node, int edge) {
+            this.node =node;
+            this.edge = edge;
         }
     }
     
     private int isonediff(String word1, String word2) {
         int count = 0;
-        for(int i=0; i<word1.length(); i++){
-        if(word1.charAt(i) != word2.charAt(i))
-            count++;}
+        for( int i = 0; i<word1.length(); i++){
+            if(word1.charAt(i) != word2.charAt(i))
+                count++;
+        }
         return count;
-        }
-        
+    }
+    
     public int solution(String begin, String target, String[] words) {
-        boolean[] visited = new boolean[words.length];
-        if (!Arrays.asList(words).contains(target)){
-            return 0;
-        }
-        Queue<Words> queue = new LinkedList<>();
-        queue.offer(new Words(begin,0));
+        boolean[]  visited = new boolean[words.length];
         
-        while(!queue.isEmpty()) {
-            Words next = queue.poll();
-            String word = next.word;
-            int count = next.count;
+        Queue<Words> q = new LinkedList<>();
+        q.offer(new Words(begin, 0));
+        
+        while(!q.isEmpty()) {
+            Words cur = q.poll();
+            String node = cur.node;
+            int edge = cur.edge;
             
-            if(word.equals(target)){
-                return count;
+            if(node.equals(target)){
+                return edge;
             }
-            
-            for(int i = 0; i<words.length; i++ ){
-                if(!visited[i] && isonediff(word, words[i]) == 1)
-                {  visited[i] = true;
-                queue.offer(new Words(words[i],count+1));
+            for (int i = 0; i <words.length; i++ ){
+                if(isonediff(node,words[i])==1 && !visited[i]){
+                    q.offer(new Words(words[i],edge+1));
+                    visited[i]=true;
                 }
             }
         }
