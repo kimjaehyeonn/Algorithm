4bdb281c6dbc27a35e48de7ef03d03386aa001a0
kimjaehyeonn <jhjhh023@gmail.com>
2025-05-28 23:31:34 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 10.41 ms, Memory: 92.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,49 +1,47 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int[][] wires) {
-        int answer= n;
-        for (int i =0; i< wires.length; i++ ){
-            List<List<Integer>> tree = new ArrayList<>();
-            
-            for (int j = 0; j<=n; j++) {
-tree.add(new ArrayList<>());                
-            }
-            
-            for ( int j = 0; j<wires.length; j++) {
-                if( i == j)  continue;
-                int a = wires[j][0];
-                int b = wires[j][1];
-                tree.get(a).add(b);
-                tree.get(b).add(a);              
-            }
+    int answer = n;
+        for (int i =0; i < wires.length; i++) {
+        List<List<Integer>> graph = new ArrayList<>();
         
-            int count = bfs(1, tree, n);
-            
-            int diff = Math.abs(n - count - count);
-            answer = Math.min(answer, diff);
+        for(int j=0; j<=n; j++) {
+            graph.add(new ArrayList<>());
         }
+        
+        for(int j =0; j<wires.length; j++){
+            if (i==j) continue;
+            int a = wires[j][0];
+            int b = wires[j][1];
+            
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+        
+        int count = bfs(1, graph, n);
+        
+        int diff = Math.abs(n -count - count);
+            
+            answer = Math.min(answer, diff);
+        
+    }
         return answer;
     }
-    private int bfs(int start, List<List<Integer>> tree, int n ) {
-        boolean[] visited = new boolean[n+1];
-        
+    
+    private int bfs(int start, List<List<Integer>> graph, int n ){
         Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[n+1];
+        int count = 1;
         queue.offer(start);
         visited[start] = true;
-        int count =1; 
         
-        while(!queue.isEmpty()) {
-            int now = queue.poll();
-            for ( int next: tree.get(now)) {
-                if(!visited[next]) {
-                    visited[next] =true;
-                    queue.offer(next);
-                    count++;
-                }
+        while(!queue.isEmpty()){
+            int cur = queue.poll();
+            for(int next: graph.get(cur) ){
+            if(!visited[next]){                visited[next]= true;
+                queue.offer(next);
+                count++;}
             }
-            
-        }
-    return count;
-    }
-    
 }
+               return count;
+ }}
