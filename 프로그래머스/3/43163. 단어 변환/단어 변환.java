80ff183a415babc0de7717f9721b06374a367ac1
kimjaehyeonn <jhjhh023@gmail.com>
2025-05-29 11:23:23 +0900

    [level 3] Title: 단어 변환, Time: 1.17 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,45 +2,54 @@ import java.util.*;
 class Solution {
     
     class Words{
-    String node;
-        int edge;
-        Words(String node, int edge) {
-            this.node =node;
-            this.edge = edge;
+        String word;
+        int count;
+        
+        Words(String word, int count){
+            this.word = word;
+            this.count = count;
         }
     }
     
-    private int isonediff(String word1, String word2) {
-        int count = 0;
-        for( int i = 0; i<word1.length(); i++){
-            if(word1.charAt(i) != word2.charAt(i))
-                count++;
-        }
-        return count;
-    }
     
     public int solution(String begin, String target, String[] words) {
-        boolean[]  visited = new boolean[words.length];
+        boolean[] visited = new boolean[words.length];
+        Queue<Words> queue = new LinkedList<>();
+        queue.offer(new  Words(begin, 0));
         
-        Queue<Words> q = new LinkedList<>();
-        q.offer(new Words(begin, 0));
-        
-        while(!q.isEmpty()) {
-            Words cur = q.poll();
-            String node = cur.node;
-            int edge = cur.edge;
-            
-            if(node.equals(target)){
-                return edge;
-            }
-            for (int i = 0; i <words.length; i++ ){
-                if(isonediff(node,words[i])==1 && !visited[i]){
-                    q.offer(new Words(words[i],edge+1));
-                    visited[i]=true;
-                }
-            }
+        if (!Arrays.asList(words).contains(target)) {
+            return 0;
         }
         
-        return 0;
+        while(!queue.isEmpty()) {
+            Words next = queue.poll();
+            String word = next.word;
+            int count = next.count;
+            
+            if(word.equals(target)){
+                return count;
+            }
+            
+            for ( int i=0; i<words.length; i++ ){   
+            
+            if (isonediff(word,words[i]) && !visited[i]){
+                queue.offer(new Words(words[i], count+1));
+                visited[i] =true;
+    
+    
+        } 
+        }
+        
+        }
+       return 0; 
+    }
+    private boolean isonediff(String word1, String word2) {
+        int count =0;
+        for( int i=0; i <word1.length(); i++ ){
+            if(word1.charAt(i) != word2.charAt(i)) {
+                count++;
+            }
+        }
+        if (count == 1 ) {return true;} else return false;
     }
 }
