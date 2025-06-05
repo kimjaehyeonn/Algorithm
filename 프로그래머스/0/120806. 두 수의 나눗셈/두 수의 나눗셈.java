ffd40c1e4e3f199a064e4db8a56d7503504b043c
kimjaehyeonn <jhjhh023@gmail.com>
2025-06-05 13:39:13 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        double numa= num1;
+        double numb = num2;
+        double answer = numa/numb *1000;
+        return (int) answer;
+    }
+}
