7ef6b440ecd9d50b3160dbaedd7010c17ca1e6d5
kimjaehyeonn <jhjhh023@gmail.com>
2025-06-23 12:46:34 +0900

    [level 0] Title: 배열 만들기 3, Time: 10.96 ms, Memory: 89.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        List<Integer> list = new ArrayList<>();
+        for (int i =0; i< intervals.length; i++) {
+            int s = intervals[i][0];
+            int e = intervals[i][1];
+            for (int j =s; j <=e; j++) {
+                list.add(arr[j]);
+            }
+        }
+        
+        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
+        return answer;
+    }
+}
