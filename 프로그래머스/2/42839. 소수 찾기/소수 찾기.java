9668b7dc26aa201295e6ee3553c51a837a1242f6
kimjaehyeonn <jhjhh023@gmail.com>
2025-07-03 10:30:59 +0900

    [level 2] Title: 소수 찾기, Time: 19.10 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,37 +1,31 @@
 import java.util.*;
 class Solution {
-    
-    Set<Integer> primeSet;
-    
+    Set<Integer> primeset;
     public int solution(String numbers) {
+        primeset = new HashSet<>();
         boolean[] visited = new boolean[numbers.length()];
-        primeSet = new HashSet<>();
         backtrack(numbers, visited, "");
-        return primeSet.size();
+        return primeset.size();
     }
-    
-    void backtrack (String numbers, boolean[] visited, String cur) {
-        if(cur.length() != 0 ) {
+    void backtrack(String numbers, boolean[] visited, String cur) {
+        if(cur.length() !=0 ){
             int num = Integer.parseInt(cur);
-            if(isPrime(num)) {
-                primeSet.add(num);
+            if(isprime(num)) primeset.add(num);
+        }
+        
+        for(int i =0; i<numbers.length();i++ ){
+            if(!visited[i]){
+                visited[i] = true;
+                backtrack(numbers, visited, cur + numbers.charAt(i));
+                visited[i]= false;
             }
         }
-        for (int i = 0; i < numbers.length(); i++  ) {
-            if(!visited[i])
-            {visited[i] = true;
-            backtrack(numbers, visited, cur + numbers.charAt(i));
-            visited[i] = false;
-        }}
-        
     }
-    
-    boolean isPrime(int num) {
-        if (num<2) {return false;}
-        for (int i = 2; i*i <=num; i++ ){
-            if(num%i==0) {return false;}
+    boolean isprime (int num ){
+        if(num<2) return false;
+        for(int i=2; i*i<=num; i++ ){
+            if(num%i == 0 )return false;
         }
         return true;
     }
-    
 }
