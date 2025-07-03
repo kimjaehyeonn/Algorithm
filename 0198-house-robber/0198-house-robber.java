458ef64fcd8ea9f9e04c7826656717d887e4096c
kimjaehyeonn <jhjhh023@gmail.com>
2025-07-03 11:17:11 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,12 +1,12 @@
-import java.util.*;
 class Solution {
     public int rob(int[] nums) {
-        int curr = 0, prev = 0;
-        for (int num: nums){
-            int temp = curr;
-            curr = Math.max(curr, prev+num);
-            prev = temp;
-        }
-        return curr;
-    }
+        int cur = 0; 
+        int prev =0;
+        for (int num :nums){
+           int temp = cur;
+           cur = Math.max(cur, prev+num);
+           prev = temp;
+        } 
+        return cur
+            }
 }
